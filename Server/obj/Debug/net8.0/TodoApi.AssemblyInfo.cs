00991c191268ca5d973756802cc47c7dcda048dc
ettylax <ettylax@gmail.com>
2025-03-19 11:51:35 +0200

    Add .gitignore

--- Server/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f01d3626ece94dc67e31157204a326c4238519f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+669fab1631a71de1ab4c832cd8fafaf0d32af918")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
