0acb3cf94f94ac02e0cf34ce47f8a78d1a463b8c
ettylax <ettylax@gmail.com>
2025-03-19 11:06:31 +0200

    good!

--- Server/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a618030453827495099429e5d89530123cf8cf96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ba6da1ccad5017fcf12341e3de872227938ca5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
